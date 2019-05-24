a81843ac4d479cbec3336d7cf387e6ca08898203
remedialchaostheory <remedialchaostheory@users.noreply.github.com>
2019-05-24 15:32:33 -0700

    Adjust site title down and menu bar up. Adjust collapsed menu button (hamburger) up for narrow screens. Add TODOs

--- functions.php
+++ functions.php
@@ -21,5 +21,4 @@ function child_enqueue_styles() {
 	wp_enqueue_style( 'astra-child-theme-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_THEME_VERSION, 'all' );
 
 }
-
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
